2aa822c0394779c3151bd803719e154891f8b551
Sean Siganoff <nationwideclassics@yahoo.com>
2022-09-04 18:41:47 -0400

    dashboard style btn fix

--- src/components/pages/dashboard/dashboard.js
+++ src/components/pages/dashboard/dashboard.js
@@ -9,21 +9,38 @@ const Dashboard = () => {
     <div className='dashboard-container'>
       <div className='dashboard-overlay'>
         <h1>DASHBOARD</h1>
-        <Link to="/dashboard/update-project">
-          <button>View projects</button>
-        </Link>
-        <Link to="/dashboard/new-project">
-          <button>Create a new project</button>
-        </Link>
-        <Link to="/dashboard/view-bugs">
-          <button>View bugs</button>
-        </Link>
-        <Link to="/dashboard/new-bug">
-          <button>Create a new bug</button>
-        </Link>
-        <Link to="/dashboard/update-user">
-          <button>Update user info</button>
-        </Link>
+
+        <div className='dashboard-btn-container'>
+          <div className='dashboard-btn'>
+            <Link to="/dashboard/update-project">
+              <button>View projects</button>
+            </Link>
+          </div>
+
+          <div className='dashboard-btn'>
+          <Link to="/dashboard/new-project">
+            <button>Create a new project</button>
+          </Link>
+          </div>
+
+          <div className='dashboard-btn'>
+            <Link to="/dashboard/view-bugs">
+              <button>View bugs</button>
+            </Link>
+          </div>
+
+          <div className='dashboard-btn'>
+            <Link to="/dashboard/new-bug">
+              <button>Create a new bug</button>
+            </Link>
+          </div>
+          
+          <div className='dashboard-btn'>
+            <Link to="/dashboard/update-user">
+              <button>Update user info</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
