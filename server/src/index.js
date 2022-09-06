5084ef26b1a0e184a6e89abd7ffbcb0107acf4bf
Sean Siganoff <nationwideclassics@yahoo.com>
2022-09-06 19:30:12 -0400

    fixing redirect catch-all mistake

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ app.use('/api', authRoutes)
 app.use(express.static(path.resolve(__dirname, "../../build")))
 
 
-app.use('*', (req, res) => {
+app.use('/*', (req, res) => {
     res.redirect('/dashboard');
 })
 
