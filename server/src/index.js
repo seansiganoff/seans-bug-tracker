f40e6e23febb566846498f1fb10457ae5df9520e
Sean Siganoff <nationwideclassics@yahoo.com>
2022-09-06 21:04:43 -0400

    backend bug

--- server/src/index.js
+++ server/src/index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.resolve(__dirname, "../../build")))
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../../public/index.html'), function(err) {
+    res.sendFile(path.join(__dirname, '../../../build'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
