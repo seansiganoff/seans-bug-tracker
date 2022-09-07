c9faa9c6a21c752afb0bdb26fe0952cf3c9f35db
Sean Siganoff <nationwideclassics@yahoo.com>
2022-09-06 20:16:54 -0400

    trying to fix redirect bug

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ app.use('/api', authRoutes)
 app.use(express.static(path.resolve(__dirname, "../../build")))
 
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.redirect('/dashboard');
 })
 
